24daff8f2c682ecfacb3c2a31bb41b52653b7acf
Mohsin Azam <mohsin.afridi91@gmail.com>
2024-04-21 00:59:15 +0400

    Add project files.

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Rio.Mvc.Web.Controllers
+{
+    public class MenuController :  Sitecore.Mvc.Controllers.SitecoreController
+    {
+        // GET: Menu
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rio.Mvc.Web.MvcApplication" Language="C#" %>
